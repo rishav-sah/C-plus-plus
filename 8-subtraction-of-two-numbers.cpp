ee46f71b28714cb57996fcc9fb593602e5250522
killer-rishav <rishavsah4u@gmail.com>
2021-12-15 15:02:56 +0545

    Subtraction of two numbers

---
+++ 8-subtraction-of-two-numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  float a = 0, b = 0, sub;
+
+  cout << "Enter two numbers:" << endl;
+  cin >> a >> b;
+
+  sub = a - b;
+
+  cout << a << " - " << b << " = " << sub << endl;
+
+  return 0;
+}
